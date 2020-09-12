afe0ad3c72032dd37239efcb5086b9bac970524a
Sujit Joshi <joshi.sujit020386@gmail.com>
2020-09-12 17:52:13 -0400

    1) Adding solution. 2) Adding project files.

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,46 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut char_vec = Vec::new();
+
+    for c in string.chars() {
+        match c {
+            '[' => char_vec.push('['),
+            ']' => {
+                let last = char_vec.last();
+                if last.is_none() || *last.unwrap() != '[' {
+                    return false;
+                } else {
+                    char_vec.pop();
+                }
+            },
+            '{' => char_vec.push('{'),
+            '}' => {
+                let last = char_vec.last();
+                if last.is_none() || *last.unwrap() != '{' {
+                    return false;
+                } else {
+                    char_vec.pop();
+                }
+            },
+            '(' => char_vec.push('('),
+            ')' => {
+                let last = char_vec.last();
+                if last.is_none() || *last.unwrap() != '(' {
+                    return false;
+                } else {
+                    char_vec.pop();
+                }
+            },
+            _ => {}
+        }
+    }
+
+    char_vec.is_empty()
+}
+
+fn is_valid(chech_char: &char, last_char: Option<&char>) -> bool {
+    if last_char.is_none() || *last_char.unwrap() != *chech_char {
+        false
+    } else {
+        true
+    }
 }
