38c8724326c1ef68c91e2043da0a5107aa27baf9
Sujit Joshi <joshi.sujit020386@gmail.com>
2020-09-13 15:47:27 -0400

    Adding files.

---
+++ rust/saddle-points/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn find_saddle_points(input: &[Vec<u64>]) {
+    let rows = input.len();
+    if rows > 0 {
+        let columns = input[0].len();
+        let mut bit_vec = vec![vec![false; columns]; rows];
+    }
+}
+
+pub fn find_max_in_row(input: &[Vec<u64>], bit_vec: &mut Vec<Vec<bool>>) {
+    for (index, row) in input.iter().enumerate() {
+        //find index of max in row
+    }
+}
+
+pub fn find_min_in_col(input: &[Vec<u64>]) -> usize {
+
+}
