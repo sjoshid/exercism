c1bec81901cb351b7c14293b2e0ad9fb8f738d76
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-02-09 21:43:12 -0500

    Solution

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,22 +1,26 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+use std::ops::Div;
+
+pub const EARTH_SECONDS: f64 = 31_557_600.0;
+
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    earth_years: f64
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        let s= s as f64;
+        Duration {
+            earth_years: s.div(EARTH_SECONDS)
+        }
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
 pub struct Mercury;
@@ -28,11 +32,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 0.2408467
+    }
+}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 0.61519726
+    }
+}
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 1.0
+    }
+}
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 11.862615
+    }
+}
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 29.447498
+    }
+}
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 84.016846
+    }
+}
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        d.earth_years / 164.79132
+    }
+}
