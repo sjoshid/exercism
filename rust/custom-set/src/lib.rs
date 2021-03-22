77473847bd8c49d6a0b07e544c3353d8adb19fb2
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-03-22 16:56:07 -0400

    Better solution with internal vec

--- rust/custom-set/src/lib.rs
+++ rust/custom-set/src/lib.rs
@@ -1,43 +1,36 @@
 #[derive(Debug)]
-pub struct CustomSet<T: Copy + Eq  + Ord> {
+pub struct CustomSet<T: Copy + Eq + Ord> {
 	internal: Vec<T>
 }
 
-impl<T: Copy + Eq  + Ord> PartialEq for CustomSet<T> {
+impl<T: Copy + Eq + Ord> PartialEq for CustomSet<T> {
 	fn eq(&self, other: &Self) -> bool {
 		self.internal.len() == other.internal.len() && self.internal.iter().all(|k| other.internal.contains(k))
 	}
 }
 
-impl<T: Copy + Eq  + Ord> CustomSet<T> {
+impl<T: Copy + Eq + Ord> CustomSet<T> {
 	pub fn new(input: &[T]) -> Self {
+		let mut internal = input.to_vec();
+		internal.sort();
+		internal.dedup();
 		CustomSet {
-			internal: input.to_vec()
+			internal
 		}
 	}
 
 	pub fn contains(&self, element: &T) -> bool {
-		self.internal.binary_search(element).is_ok()
+		self.internal.contains(element)
 	}
 
 	pub fn add(&mut self, e: T) {
-		if self.internal.binary_search(&e).is_err() {
+		if !self.internal.contains(&e) {
 			self.internal.push(e);
 		}
 	}
 
 	pub fn is_subset(&self, other: &Self) -> bool {
-		match (self.internal.len(), other.internal.len()) {
-			(0, 0) | (0, _) => {
-				true
-			},
-			(_, 0) => {
-				false
-			}
-			(_, _) => {
-				self.internal.iter().all(|k| other.internal.contains(k))
-			}
-		}
+		self.internal.iter().all(|k| other.internal.contains(k))
 	}
 
 	pub fn is_empty(&self) -> bool {
@@ -45,74 +38,27 @@ impl<T: Copy + Eq  + Ord> CustomSet<T> {
 	}
 
 	pub fn is_disjoint(&self, other: &Self) -> bool {
-		match (self.internal.len(), other.internal.len()) {
-			(0, 0) | (0, _) | (_, 0) => {
-				true
-			},
-			(_, _) => {
-				!self.internal.iter().any(|k| other.internal.contains(k))
-			}
-		}
+		!self.internal.iter().any(|k| other.internal.contains(k))
 	}
 
 	pub fn intersection(&self, other: &Self) -> Self {
-		match (self.internal.len(), other.internal.len()) {
-			(0, _) | (_, 0) => {
-				CustomSet {
-					internal: Vec::new()
-				}
-			},
-			(_, _) => {
-				let mut intersection = Vec::new();
-				for element in self.internal.iter() {
-					if other.internal.contains(element) {
-						intersection.push(*element);
-					}
-				}
-				CustomSet {
-					internal: intersection
-				}
-			}
+		let mut intersection: Vec<T> = self.internal.iter().filter(|e| other.internal.contains(*e)).cloned().collect();
+
+		CustomSet {
+			internal: intersection
 		}
 	}
 
 	pub fn difference(&self, other: &Self) -> Self {
-		match (self.internal.len(), other.internal.len()) {
-			(0, _) => {
-				Self::new(&[])
-			},
-			(_, 0) => {
-				Self::new(self.internal.as_slice())
-			},
-			(_, _) => {
-				let mut difference = Vec::new();
-				for element in self.internal.iter() {
-					if !other.internal.contains(element) {
-						difference.push(*element);
-					}
-				}
-				CustomSet {
-					internal: difference
-				}
-			}
+		let mut intersection: Vec<T> = self.internal.iter().filter(|e| !other.internal.contains(*e)).cloned().collect();
+
+		CustomSet {
+			internal: intersection
 		}
 	}
 
 	pub fn union(&self, other: &Self) -> Self {
-		let mut internal = Vec::new();
-		for k in self.internal.iter() {
-			if !internal.contains(k) {
-				internal.push(*k);
-			}
-		}
-		for k in other.internal.iter() {
-			if !internal.contains(k) {
-				internal.push(*k);
-			}
-		}
-
-		CustomSet {
-			internal
-		}
+		let internal: Vec<T> = self.internal.iter().chain(other.internal.iter()).cloned().collect();
+		CustomSet::new(&internal)
 	}
 }
