e766c79305d8d82ae998cb4608c19927d5551f2a
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-03-31 21:37:58 -0400

    Second attempt with a good solution. I like it. I'll move on to decode

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,32 +1,20 @@
 pub fn encode(source: &str) -> String {
-    if source.is_empty() {
-        String::from("")
-    } else {
-        let mut source = source.to_string();
-        source.push('0');
+    let source = source.to_owned() + "0";
+    let mut count = 1;
+    let mut result = String::from("");
 
-        let mut cc = String::from(source.get(0..1).unwrap());
-        let mut count = 1;
-        let mut result = String::from("");
-
-        for e in source.chars().skip(1) {
-            let c = e.to_string();
-
-            if c == cc {
-                count += 1;
-            } else if count == 1 {
-                result += format!("{}", cc).as_str();
-                count = 1;
-                cc = c;
-            } else {
-                result += format!("{}{}", count, cc).as_str();
-                count = 1;
-                cc = c;
-            }
+    for (f, s) in source.chars().zip(source.chars().skip(1)) {
+        if f == s {
+            count += 1;
+        } else if count == 1 {
+            result = format!("{}{}", result, f);
+        } else {
+            result = format!("{}{}{}", result, count, f);
+            count = 1;
         }
-
-        result
     }
+
+    result
 }
 
 pub fn decode(source: &str) -> String {
