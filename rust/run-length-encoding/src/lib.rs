bd5d2ab0c5d6684a288f5483e9dada88d2b5429e
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-03-31 20:52:51 -0400

    First attempt

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,5 +1,32 @@
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    if source.is_empty() {
+        String::from("")
+    } else {
+        let mut source = source.to_string();
+        source.push('0');
+
+        let mut cc = String::from(source.get(0..1).unwrap());
+        let mut count = 1;
+        let mut result = String::from("");
+
+        for e in source.chars().skip(1) {
+            let c = e.to_string();
+
+            if c == cc {
+                count += 1;
+            } else if count == 1 {
+                result += format!("{}", cc).as_str();
+                count = 1;
+                cc = c;
+            } else {
+                result += format!("{}{}", count, cc).as_str();
+                count = 1;
+                cc = c;
+            }
+        }
+
+        result
+    }
 }
 
 pub fn decode(source: &str) -> String {
