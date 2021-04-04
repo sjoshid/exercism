45e95becd5556d2ce9c5cce01faf62a9d82d6fcb
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-04-04 16:00:26 -0400

    Final attempt. Works!

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -18,5 +18,38 @@ pub fn encode(source: &str) -> String {
 }
 
 pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+    let source = "#".to_owned() + source;
+
+    let indices = source
+        .char_indices()
+        .filter_map(|(i, c)| {
+            if c.is_alphabetic() || c.is_whitespace() {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<usize>>();
+
+    let mut c = 0 as usize;
+    let mut result = String::new();
+
+    for i in indices {
+        if let Some(s) = source.get((c + 1)..i) {
+            match s.is_empty() {
+                true => {
+                    let char_with_one_count = source.get(i..(i + 1)).unwrap();
+                    result += char_with_one_count;
+                }
+                false => {
+                    if let Ok(v) = s.parse::<usize>() {
+                        let char_to_repeat = source.get(i..(i + 1)).unwrap();
+                        result += char_to_repeat.repeat(v).as_str();
+                    }
+                }
+            }
+            c = i;
+        }
+    }
+    result
 }
