27eac3b66106e96d4b1a6c02b3544d3fd2b06695
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-04-04 22:09:39 -0400

    Non generic version of binary search.

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,26 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    let mut result = None;
+    if !array.is_empty() {
+        let mut left = 0 as usize;
+        let mut right = array.len() - 1 as usize;
+
+        while left < right {
+            let half = left + ((right - left) >> 1);
+            if array[half] > key {
+                right = half;
+            } else if array[half] < key {
+                left = half + 1;
+            } else {
+                //found
+                result = Some(half);
+                break;
+            }
+        }
+
+        if result == None && array[left] == key {
+            result = Some(left);
+        }
+    }
+
+    return result;
 }
