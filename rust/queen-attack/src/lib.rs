614bff49e1739252b349000cda46c5edcb78a521
Sujit Joshi <joshi.sujit020386@gmail.com>
2021-04-13 17:00:22 -0400

    Solved in first attempt

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,44 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if (0..8).contains(&rank) && (0..8).contains(&file) {
+            Some(ChessPosition {rank, file})
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
+        Queen {
             position
-        );
+        }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let (self_rank, self_file) = (&self.position.rank, &self.position.file);
+        let (other_rank, other_file) = (&other.position.rank, &other.position.file);
+
+        match (self_rank == other_rank, self_file == other_file) {
+            (true, _) => true,
+            (_, true) => true,
+            (false, false) => {
+                let diff_file = (self_file - other_file).abs();
+                let diff_rank = (self_rank - other_rank).abs();
+
+                diff_file == diff_rank
+            }
+        }
     }
 }
